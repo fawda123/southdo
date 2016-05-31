650b916bae52f7dc0d6ff58f4423e8a527ff5116
fawda123 <mbafs2012@gmail.com>
2016-05-31 15:45:08 -0500

    rdata files for raw and evalcor results, update readme with evalcor plots

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -6,7 +6,11 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(lubridate)
-devtools::load_all('M:/docs/SWMP/WtRegDO')
+library(WtRegDO)
+# devtools::load_all('M:/docs/SWMP/WtRegDO')
+
+##
+# import data, clean up, save
 
 ##
 dum <- read.csv('ignore/Dumbarton_07022013_04282016_L1.csv') %>% 
@@ -20,10 +24,10 @@ dum <- read.csv('ignore/Dumbarton_07022013_04282016_L1.csv') %>%
   select(DateTimeStamp, Temp, Sal, DO_obs, Tide) %>% 
   mutate(DateTimeStamp = as.POSIXct(DateTimeStamp, format = '%m/%d/%Y %H:%M', tz = 'Pacific/Pitcairn'))
 
-toplo <- gather(dum, 'var', 'val', -DateTimeStamp)
-ggplot(toplo, aes(DateTimeStamp, val)) + 
-  geom_line() + 
-  facet_wrap(~var)
+# toplo <- gather(dum, 'var', 'val', -DateTimeStamp)
+# ggplot(toplo, aes(DateTimeStamp, val)) + 
+#   geom_line() + 
+#   facet_wrap(~var)
 
 ##
 new <- read.csv('ignore/Newark_04212015_05132016_L1 (1).csv') %>% 
@@ -37,25 +41,55 @@ new <- read.csv('ignore/Newark_04212015_05132016_L1 (1).csv') %>%
   select(DateTimeStamp, Temp, Sal, DO_obs, Tide) %>% 
   mutate(DateTimeStamp = as.POSIXct(DateTimeStamp, format = '%m/%d/%Y %H:%M', tz = 'Pacific/Pitcairn'))
 
-toplo <- gather(new, 'var', 'val', -DateTimeStamp)
-ggplot(toplo, aes(DateTimeStamp, val)) + 
-  geom_line() + 
-  facet_wrap(~var)
+# toplo <- gather(new, 'var', 'val', -DateTimeStamp)
+# ggplot(toplo, aes(DateTimeStamp, val)) + 
+#   geom_line() + 
+#   facet_wrap(~var)
 
+# save raw files
+save(dum, file = 'data/dum.RData', compress = 'xz')
+save(new, file = 'data/new.RData', compress = 'xz')
 
-# # run weighted regression in parallel
-# # requires parallel backend
+######
+# create objects of tidal correlation with sun angle for each site
+# newark is full data range
+# dumbarton is truncated by newark dates
+
+rm(list = ls())
+
+data(dum)
+data(new)
+
+# run weighted regression in parallel
+# requires parallel backend
 library(doParallel)
-registerDoParallel(cores = 6)
+registerDoParallel(cores = 7)
 
-# dum coords
+##
+# dumbarton evalcor
+
+# subset dumbarton by newark date range
+dtrng <- range(new$DateTimeStamp, na.rm = TRUE)
+toeval <- filter(dum, DateTimeStamp >= dtrng[1] & DateTimeStamp <= dtrng[2])
+
+# dum coords, tz
 lat <- 37.504167
 long <- -122.119444
+tz <- 'Pacific/Pitcairn'
+
+dum_evalcor <- evalcor(toeval, tz, lat, long, progress = TRUE)
+
+save(dum_evalcor, file = 'data/dum_evalcor.RData', compress = 'xz')
+
+##
+# newark evalcor
 
 # newark coords
-# lat <- 37.51338333
-# long <- -122.0821
+lat <- 37.51338333
+long <- -122.0821
 tz <- 'Pacific/Pitcairn'
-devtools::load_all('M:/docs/SWMP/WtRegDO')
-toeval <- filter(dum, year(DateTimeStamp) %in% 2014)
-tmp <- evalcor(toeval, tz, lat, long, progress = TRUE)
+
+toeval <- new
+new_evalcor <- evalcor(toeval, tz, lat, long, progress = TRUE)
+
+save(new_evalcor, file = 'data/new_evalcor.RData', compress = 'xz')
--- README.md
+++ README.md
@@ -1,8 +1,26 @@
 # README
 Marcus W. Beck, beck.marcus@epa.gov  
 
+### Files
+
+All data files created from raw data in `ignore` folder using `R\dat_proc.R`
+
+`dum.RData` Dumbarton time series data from `Dumbarton_07022013_04282016_L1.csv`
+
+`dum_evalcor.RData` ggplot of tidal/sun correlations at Dumbarton
+
+`new.RData` Newark time series data from `Newark_04212015_05132016_L1 (1).csv`
+
+`new_evalcor.RData` ggplot of tidal/sun correlations at Newark
 
 
-### DO time series from southern SF Bay
 
+### Correlation between sun angle and tidal change at each site
 
+Dumbarton
+
+![](README_files/figure-html/unnamed-chunk-2-1.png)
+
+Newark
+
+![](README_files/figure-html/unnamed-chunk-3-1.png)
